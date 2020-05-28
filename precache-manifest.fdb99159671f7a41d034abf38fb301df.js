3fd019467fc12b9db19b2c7d4f1049feef05a9a9
Ivan Flores <ivanflores@Ivans-MacBook-Pro.local>
2020-05-27 22:36:05 -0700

    Updates

---
+++ precache-manifest.fdb99159671f7a41d034abf38fb301df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98fc760ab3b33f6a0558e004767a93c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43ec910cb8a1b6beb485",
+    "url": "/monsters-rolodex/static/css/main.edd3c27d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ec910cb8a1b6beb485",
+    "url": "/monsters-rolodex/static/js/main.a94bb49b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
